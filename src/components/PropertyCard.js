5605581bb80dd2aee515d6a6fd06e1a08cc1c208
utkarshpathak2504 <utkarshpathak2504@gmail.com>
2021-10-08 23:36:38 +0530

    added property card in home

--- src/components/PropertyCard.js
+++ src/components/PropertyCard.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Skeleton, Switch, Card, Avatar } from "antd";
+import { Row, Col, Button, Space } from "antd";
+
 const { Meta } = Card;
 // import homes from "../";
 // import "./PropertyCards.css";
@@ -9,19 +11,63 @@ function PropertyCard() {
     <div>
       <Card
         style={{
-          width: 800,
+          width: 1200,
           margin: 16,
           alignItems: "center",
-          height: 300,
+
           padding: "0%",
         }}
       >
-        <img
+        {/* <img
           alt="example"
           src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
           style={{ width: "50%", margin: "2%" }}
         />
-        <h3>Property Name</h3>
+        <h3>Property Name</h3> */}
+        <Row style={{ padding: 0 }}>
+          <Col span={24}>
+            <Row>
+              <Col span={10}>
+                <img
+                  alt="example"
+                  src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                  style={{ width: "100%", margin: "2%", padding: 0 }}
+                />
+              </Col>
+              <Col span={14} style={{ textAlign: "center" }}>
+                <h1>Property Name</h1>
+                <h2>Yaha located hai bhai mast jagah hai</h2>
+                <Row>
+                  <Col span={12}>
+                    <h2>
+                      {" "}
+                      Itta total Paisa
+                      <br />
+                      $50000
+                    </h2>
+                  </Col>
+                  <Col span={12}>
+                    <h2>
+                      {" "}
+                      Itta total Paisa <br />
+                      $200
+                    </h2>
+                  </Col>
+                </Row>
+
+                <Button
+                  type="primary"
+                  shape="round"
+                  size="large"
+                  style={{ width: "40%", marginTop: "5%" }}
+                >
+                  {" "}
+                  Buy Now Bro
+                </Button>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
       </Card>
     </div>
   );
